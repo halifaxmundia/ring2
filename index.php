8cc081f63914c903e1f8822a24fbb79835c13a80
halifaxmundia <halifaxmundia@gmail.com>
2024-01-30 22:31:07 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial scale=1.0">
+    <meta name="description" content="Your Apple Store">
+    <meta name="keywords"content="iPhones,iMacs,MacBooks,iPods">
+    <link rel="stylesheet" href="./CSS/styles.css">
+    <title>Ring</title>
+</head>
+<body>
+    <nav>
+        <div class="brand">Ring</div>
+        <div class="links">
+            <a data-active="index" href="index.php">Home</a>
+            <a data-active="about" href="about.php">About</a>
+            <a data-active="contact" href="contact.php">Contact</a>
+        </div>
+    </nav>
+    <header></header>
+
+    <main></main>
+
+    <footer></footer>
+    
+    <script src="javascript/script.js"></script>
+</body>
+</html>
